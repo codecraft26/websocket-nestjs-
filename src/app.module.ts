dab681f696947290bcaf70e0e1991ac71c433f63
codecraft26 <gaman0221@gmail.com>
2023-03-24 12:54:42 +0530

    code correction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+
+import {ConfigModule} from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -28,11 +28,8 @@ import { UsersModule } from './users/users.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
 
     }),
-    AuthModule,
+  ,
     UsersModule
-    
-
-
   ],
   controllers: [AppController],
   providers: [AppService],
